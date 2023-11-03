83110593e0f2641602945fdb28f6fb0a89abf0c4
IamBr4vo <bravo9386@gmail.com>
2023-11-03 16:05:29 -0600

    add constructors

--- ProjectTwo/src/model/Event.java
+++ ProjectTwo/src/model/Event.java
@@ -1,8 +1,9 @@
-
 package model;
+
 import java.util.Date;
 
 public class Event {
+
     private int id;
     private String name;
     private String description;
@@ -13,61 +14,67 @@ public class Event {
     private double price;
     private int room;
     private int place_id;
-    
-    public Event (int id, String name, String description, Date date, String address, String city, int postal_code, double price, int room, int place_id){
-     this.id = id;
-     this.name = name;
-     this.description = description;
-     this.date = date;
-     this.address = address;
-     this.city = city;
-     this.postal_code = postal_code;
-     this.price = price;
-     this.room = room;
-     this.place_id = place_id;
+
+    public Event() {
     }
 
-  
+    public Event(String name, String description, Date date, String address, String city, int postal_code, double price, int room, int place_id) {
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.address = address;
+        this.city = city;
+        this.postal_code = postal_code;
+        this.price = price;
+        this.room = room;
+        this.place_id = place_id;
+    }
+
+    public Event(int id, String name, String description, Date date, String address, String city, int postal_code, double price, int room, int place_id) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.address = address;
+        this.city = city;
+        this.postal_code = postal_code;
+        this.price = price;
+        this.room = room;
+        this.place_id = place_id;
+    }
+
     public int getId() {
         return id;
     }
 
-  
     public void setId(int id) {
         this.id = id;
     }
 
-   
     public String getName() {
         return name;
     }
 
-    
     public void setName(String name) {
         this.name = name;
     }
 
-   
     public String getDescription() {
         return description;
     }
 
-   
     public void setDescription(String description) {
         this.description = description;
     }
 
-    
     public Date getDate() {
         return date;
     }
 
-    
     public void setDate(Date date) {
         this.date = date;
     }
 
-   
     public String getAddress() {
         return address;
     }
@@ -76,37 +83,30 @@ public class Event {
         this.address = address;
     }
 
-   
     public String getCity() {
         return city;
     }
 
-    
     public void setCity(String city) {
         this.city = city;
     }
 
-   
     public int getPostal_code() {
         return postal_code;
     }
 
-    
     public void setPostal_code(int postal_code) {
         this.postal_code = postal_code;
     }
 
-    
     public double getPrice() {
         return price;
     }
 
-   
     public void setPrice(double price) {
         this.price = price;
     }
 
-    
     public int getRoom() {
         return room;
     }
@@ -115,14 +115,11 @@ public class Event {
         this.room = room;
     }
 
-    
     public int getPlace_id() {
         return place_id;
     }
 
-    
     public void setPlace_id(int place_id) {
         this.place_id = place_id;
     }
-    
 }
